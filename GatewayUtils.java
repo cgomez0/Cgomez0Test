5474c6a23009bbf342518816e63dd242f9c2f7f7
Cesar Gomez Pinto <cgomez@takion.es>
2014-12-10 18:03:19 +0100

    Uploading GatewayUtils
    It's a test.

---
+++ GatewayUtils.java
@@ -0,0 +1,16 @@
+package com.takion.p2pe.tgateway.utils;
+
+import java.util.Random;
+
+public class GatewayUtils {
+	public static String generateAuthId()
+	{
+		String authId = "";
+		
+		Random generator = new Random(System.currentTimeMillis());
+		
+		authId = String.valueOf(generator.nextInt(900000)+100000);
+		
+		return authId;
+	}
+}
